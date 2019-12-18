e294f0c87ecae9f38e8500820d38b3ab3efd95bf
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2019-12-18 13:20:44 +0000

    updated to version 0.9.6

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,8 +10,8 @@ class restic(
   $restic_upload_limit              = '204800',
   $restic_download_limit            = '204800',
   $restic_path                      = '/opt/restic',
-  $restic_binary                    = 'restic_0.9.5_linux_amd64',
-  $restic_download_url              = 'https://github.com/restic/restic/releases/download/v0.9.5/restic_0.9.5_linux_amd64.bz2',
+  $restic_binary                    = 'restic_0.9.6_linux_amd64',
+  $restic_download_url              = 'https://github.com/restic/restic/releases/download/v0.9.6/restic_0.9.6_linux_amd64.bz2',
   $restic_pre_command               = '',
   $restic_keep_prune_job            = true,
   $restic_keep_prune_job_random     = true,    # if enabled a random hour and weekday will be taken in which a prune and forget job wil run instead of backup. 
